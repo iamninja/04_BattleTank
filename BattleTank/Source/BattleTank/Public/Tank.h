abb6809c96dfa641badfdb2f7f260b5432229774
Vagios Vlachos <bagios234@gmail.com>
2017-06-06 14:20:51 +0300

    BT126 Create default subobjects in C++

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h" // Must be the last include 
 
 UCLASS()
@@ -20,6 +21,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:	
 	// Called every frame
